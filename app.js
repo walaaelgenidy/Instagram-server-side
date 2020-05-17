7d92739760b277c985a3f8099496c68d84e256c3
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-05-17 22:40:21 +0200

    add reply and delete message

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express()
 const Port = 5000;
-const {URL} = require('./config/keys')
+// const {URL} = require('./config/keys')
 
-
+const URL =  "mongodb+srv://walaa:<2ptsqUcY9PMMH6Lq>@cluster0-xmvda.mongodb.net/test?retryWrites=true&w=majority";
+  
 //mongo connections
 try {
     mongoose.connect( URL,{
@@ -24,7 +25,7 @@ require('./models/message')
 
 app.use(express.json());   // parse application/json
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded\
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 app.use(require('./routes/auth'))
 app.use(require('./routes/message'))
--- routes/message.js
+++ routes/message.js
@@ -5,7 +5,7 @@ const requireLogin = require('../middleware/requireLogin')
 const Message =mongoose.model("Message")
 
 //to veiw all messages
-router.get('./allMessages' ,requireLogin, (res,req)=>{
+router.get('/allMessages' ,requireLogin, (res,req)=>{
     Message.find()
     .populate("postedBy","_id name") //show the details of the user
     .then(messages=>{
@@ -15,17 +15,18 @@ router.get('./allMessages' ,requireLogin, (res,req)=>{
         console.log(err)
     })
 })
-
+ 
 //to create a message
-router.post('./createMessage' ,requireLogin, (req,res)=>{
-    const {title , body} = req.body
-    if(!title || !body){
+router.post('/CreateMessage' ,requireLogin, (req,res)=>{
+    const {title , body,pic} = req.body
+    if(!title || !body || !pic){
         res.status(422).json({err:"please fill all field"})
         }
         req.user.password = undefined
          const message = new Message({
              title,
              body,
+             photo:pic,
              postedBy: req.user
          })
          message.save().then(result=>{
@@ -37,7 +38,7 @@ router.post('./createMessage' ,requireLogin, (req,res)=>{
 })
 
 //messages created by user
-router.get('./myMessages',requireLogin,(req,res)=>{
+router.get('/myMessages',requireLogin,(req,res)=>{
     Message.find({postedBy:req.user._id}) //get all messages of the user who logged in
     .populate("postedBy","_id name") //show the details of the user
     .then(myMessage=>{
@@ -48,4 +49,45 @@ router.get('./myMessages',requireLogin,(req,res)=>{
     })
 })
 
+
+//to make a reply
+router.put('/reply',requireLogin,(req,res)=>{
+    const reply = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Message.findByIdAndUpdate(req.body.mesaageId,{
+        $push:{replies:reply}
+    },{
+        new:true
+    })
+    .populate("replies.postedBy","_id name")
+    .populate("postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+ //to delete a message
+router.delete('/deletemessage/:mesaageId',requireLogin,(req,res)=>{
+    Message.findOne({_id:req.params.mesaageId})
+    .populate("postedBy","_id")
+    .exec((err,message)=>{
+        if(err || !message){
+            return res.status(422).json({error:err})
+        }
+        if(message.postedBy._id.toString() === req.user._id.toString()){
+              message.remove()
+              .then(result=>{
+                  res.json(result)
+              }).catch(err=>{
+                  console.log(err)
+              })
+        }
+    })
+})
 module.exports= router
